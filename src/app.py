2df126b1d2b38b82cb92cbcaa5afcb3a9825b80d
Kairo Silveira <kairo.silveira.contato@gmail.com>
2022-11-18 17:56:10 +0100

    fix app.py log model before

--- src/app.py
+++ src/app.py
@@ -3,6 +3,12 @@ from flask import Flask,request
 import mlflow
 import pandas as pd
 
+
+mlflow.set_experiment("House Pricing Regression")
+last_run=dict(mlflow.search_runs().sort_values(by='start_time',ascending=False).iloc[0])
+artifact_uri=last_run['artifact_uri']
+model=mlflow.sklearn.load_model(artifact_uri+'/house_price_regression')
+
 app = Flask(__name__)
 
 @app.route("/predict", methods = ["POST"])
@@ -14,11 +20,6 @@ def predict():
     preparator = RegressionModel()
     df = preparator.create_yr_no_renovated(df)
     df = preparator.remov_unimportant_cols(df,["yr_built", "date", "yr_renovated"])
-    
-    mlflow.set_experiment("House Pricing Regression")
-    last_run=dict(mlflow.search_runs().sort_values(by='start_time',ascending=False).iloc[0])
-    artifact_uri=last_run['artifact_uri']
-    model=mlflow.sklearn.load_model(artifact_uri+'/house_price_regression')
     predictions=model.predict(df)
 
     return list(predictions)
