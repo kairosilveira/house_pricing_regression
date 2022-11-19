55438b0cb23934fbd4b32d06b21de998a39a6ce3
Kairo Silveira <kairo.silveira.contato@gmail.com>
2022-11-19 12:02:57 +0100

    load model out of the function in app.py

--- app.py
+++ app.py
@@ -3,17 +3,15 @@ from flask import Flask,request
 import pandas as pd
 import mlflow
 
-
+mlflow.set_experiment("House Pricing Regression")
+last_run=dict(mlflow.search_runs().sort_values(by='start_time',ascending=False).iloc[0])
+last_model_uri = "mlruns/"+last_run['experiment_id']+"/"+ last_run["run_id"] +'/artifacts/house_price_regression'
+model=mlflow.sklearn.load_model(last_model_uri)
 
 app = Flask(__name__)
 
 @app.route("/predict", methods = ["POST"])
 def predict():
-
-    mlflow.set_experiment("House Pricing Regression")
-    last_run=dict(mlflow.search_runs().sort_values(by='start_time',ascending=False).iloc[0])
-    last_model_uri = "mlruns/"+last_run['experiment_id']+"/"+ last_run["run_id"] +'/artifacts/house_price_regression'
-    model=mlflow.sklearn.load_model(last_model_uri)
     data_json = request.get_json()
     data = data_json["data"]
     df = pd.DataFrame(data)
